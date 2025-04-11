8202b9b521a1eb6359bc3b159fd3a828f7a91fb8
Cristian Santos <zcriticz.dev@gmail.com>
2025-04-11 16:59:17 -0300

    chore(assets): add and organize project assets
    - Included icons, splash screens, and other common assets.
    - Organized assets into structured folders for better maintainability.

---
+++ src/screens/Register/RegisterPageFour/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function RegisterPageFour() {
+	return (
+		<View>
+			<Text>index</Text>
+		</View>
+	);
+}
