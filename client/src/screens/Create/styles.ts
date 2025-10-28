1518e251523d8b7b2c9c3b2d9728255518f6febc
Cristian Santos <zcriticz.dev@gmail.com>
2025-10-28 12:16:03 -0300

    feat: add styles for Create screen layout and components

---
+++ client/src/screens/Create/styles.ts
@@ -0,0 +1,26 @@
+import { StyleSheet, Dimensions } from "react-native";
+import { colors } from "../../styles/colors";
+
+const { width, height } = Dimensions.get("window");
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: colors.white,
+	},
+	logo: {
+		width: width * 0.4,
+		height: height * 0.07,
+		resizeMode: "contain",
+	},
+	title: {
+		fontSize: width * 0.04,
+		maxWidth: width * 0.8,
+		textAlign: "center",
+		fontWeight: "bold",
+		marginTop: height * 0.02,
+		marginBottom: height * 0.02,
+	},
+});
