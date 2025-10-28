f2b10550f8cab1b0c123483d20b6aee37d7936f8
Cristian Santos <zcriticz.dev@gmail.com>
2025-10-28 12:21:13 -0300

    feat: add styles for Button component to improve user interaction

---
+++ client/src/styles/Button/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet, Dimensions } from "react-native";
+import { colors } from "../colors";
+
+const { width, height } = Dimensions.get("window");
+
+export const buttonStyles = StyleSheet.create({
+	button: {
+		backgroundColor: colors.green,
+		width: "80%",
+		padding: height * 0.02,
+		borderRadius: 13,
+		marginTop: height * 0.04,
+	},
+	buttonText: {
+		color: colors.white,
+		fontWeight: "bold",
+		fontSize: width * 0.035,
+		textAlign: "center",
+	},
+});
