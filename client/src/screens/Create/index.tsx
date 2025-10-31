7047d75d97b2eecda7557c90f7860dc30beb713b
Cristian Santos <zcriticz.dev@gmail.com>
2025-10-31 15:50:19 -0300

    refactor: remove unused Image import from Create screen for cleaner code

--- client/src/screens/Create/index.tsx
+++ client/src/screens/Create/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { StackTypes } from "../../routes/stackRoutes";
 import { styles } from "./styles";
