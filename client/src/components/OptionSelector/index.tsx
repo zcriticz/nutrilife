d6ce8c158fcf84fb9060131a3ba540a0651ed228
Cristian Santos <zcriticz.dev@gmail.com>
2025-11-20 13:26:47 -0300

    refactor: reorder import statements in OptionSelector for improved clarity

--- client/src/components/OptionSelector/index.tsx
+++ client/src/components/OptionSelector/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
-import { Controller } from "react-hook-form";
 import { optionSelectorStyles } from "./styles";
+import { Controller } from "react-hook-form";
 
 interface Option {
 	value: string;
