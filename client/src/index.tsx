37060f85b772e6687231db5253dd9f6fdcaac016
Cristian Santos <zcriticz.dev@gmail.com>
2025-11-20 13:26:29 -0300

    refactor: remove duplicate registerRootComponent call for cleaner index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,9 @@
 import { registerRootComponent } from "expo";
 
+registerRootComponent(Index);
+
 import MainRoutes from "./routes/mainRoutes";
 
 export default function Index() {
 	return <MainRoutes />;
 }
-
-registerRootComponent(Index);
