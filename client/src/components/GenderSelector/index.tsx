7e2df09da5069d3eee450d34eebae9079f6c29cd
Cristian Santos <zcriticz.dev@gmail.com>
2025-11-20 13:26:35 -0300

    refactor: reorder import statements in GenderSelector for improved clarity

--- client/src/components/GenderSelector/index.tsx
+++ client/src/components/GenderSelector/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
-import { Controller } from "react-hook-form";
 import { genderSelectorStyles } from "../GenderSelector/styles";
+import { Controller } from "react-hook-form";
 
 interface GenderSelectorProps {
 	name: string;
