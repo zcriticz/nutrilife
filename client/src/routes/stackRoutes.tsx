20c0d1c7734db2d86b464adbee9f2590b6b8d018
Cristian Santos <zcriticz.dev@gmail.com>
2025-12-10 18:25:17 -0300

    feat: add Login and Register screens to stack navigation and update Nutrition route parameters

--- client/src/routes/stackRoutes.tsx
+++ client/src/routes/stackRoutes.tsx
@@ -4,18 +4,23 @@ import {
 } from "@react-navigation/native-stack";
 
 import Welcome from "@/screens/Welcome";
+import Login from "@/screens/Login";
+import Register from "@/screens/Register";
 import Submit from "@/screens/Submit";
 import Create from "@/screens/Create";
 import Nutrition from "@/screens/Nutrition";
+import NutritionList from "@/screens/NutritionList";
 
 const Stack = createNativeStackNavigator();
 
 type StackNavigation = {
 	Welcome: undefined;
 	Login: undefined;
+	Register: undefined;
 	Submit: undefined;
 	Create: undefined;
-	Nutrition: undefined;
+	Nutrition: { planId?: string };
+	NutritionList: undefined;
 };
 
 export type StackTypes = NativeStackNavigationProp<StackNavigation>;
@@ -29,9 +34,12 @@ export default function StackComponent() {
 			}}
 		>
 			<Stack.Screen name="Welcome" component={Welcome} />
+			<Stack.Screen name="Login" component={Login} />
+			<Stack.Screen name="Register" component={Register} />
 			<Stack.Screen name="Submit" component={Submit} />
 			<Stack.Screen name="Create" component={Create} />
 			<Stack.Screen name="Nutrition" component={Nutrition} />
+			<Stack.Screen name="NutritionList" component={NutritionList} />
 		</Stack.Navigator>
 	);
 }
