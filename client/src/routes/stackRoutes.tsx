f0ba2b9284fabd9c4c95c88b344a74abd3d86ab9
Cristian Santos <zcriticz.dev@gmail.com>
2025-12-13 12:46:32 -0300

    feat: add ForgotPassword and ResetPassword screens to navigation stack

--- client/src/routes/stackRoutes.tsx
+++ client/src/routes/stackRoutes.tsx
@@ -6,6 +6,8 @@ import {
 import Welcome from "@/screens/Welcome";
 import Login from "@/screens/Login";
 import Register from "@/screens/Register";
+import ForgotPassword from "@/screens/ForgotPassword";
+import ResetPassword from "@/screens/ResetPassword";
 import Submit from "@/screens/Submit";
 import Create from "@/screens/Create";
 import Nutrition from "@/screens/Nutrition";
@@ -17,6 +19,8 @@ type StackNavigation = {
 	Welcome: undefined;
 	Login: undefined;
 	Register: undefined;
+	ForgotPassword: undefined;
+	ResetPassword: { token?: string };
 	Submit: undefined;
 	Create: undefined;
 	Nutrition: { planId?: string };
@@ -36,6 +40,8 @@ export default function StackComponent() {
 			<Stack.Screen name="Welcome" component={Welcome} />
 			<Stack.Screen name="Login" component={Login} />
 			<Stack.Screen name="Register" component={Register} />
+			<Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+			<Stack.Screen name="ResetPassword" component={ResetPassword} />
 			<Stack.Screen name="Submit" component={Submit} />
 			<Stack.Screen name="Create" component={Create} />
 			<Stack.Screen name="Nutrition" component={Nutrition} />
